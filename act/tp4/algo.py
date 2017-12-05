1aa377cd02221a678969fd7b9add6f3940d48499
HugoAlder <hugoalder0@gmail.com>
2017-12-05 18:43:56 +0100

    Bon ca marche plus ou moins mais c'est pas top top non plus quannd même

--- act/tp4/algo.py
+++ act/tp4/algo.py
@@ -10,20 +10,20 @@ class Job:
         self.nb_machines = nb_machines
         self.length = length
         self.start = None
-        self.machine = None
+        self.machines = []
 
     def __repr__(self):
-        return "({}, {}, {}, {}, {})".format(self.arrival, self.nb_machines, self.length, self.start, self.machine)
+        return "({}, {}, {}, {}, {})".format(self.arrival, self.nb_machines, self.length, self.start, self.machines)
 
 def get_data(filename):
     with open(filename) as f:
-        m = int(next(f))
-        n = int(next(f))
-        t = []
-        for i in range(0, n):
+        nb_machines = int(next(f))
+        nb_jobs = int(next(f))
+        jobs = []
+        for i in range(0, nb_jobs):
             line = next(f).split()
-            t.append(Line(int(line[0]), int(line[1]), int(line[2])))
-    return (m, n, t)
+            jobs.append(Job(int(line[0]), int(line[1]), int(line[2])))
+    return (nb_machines, nb_jobs, jobs)
 
 def sort_size(t):
     return sorted(t, key=operator.attrgetter('nb_machines'))
@@ -31,27 +31,43 @@ def sort_size(t):
 def sort_length(t):
     return sorted(t, key=operator.attrgetter('length'))
 
-def get_available(job, last_pos):
+def best_pos(job, last_pos):
     '''
     Machines dispos pour config c
     '''
-    Counter()
+    size = Counter(last_pos)
 
-def schedule(m, t):
+    for el in size.elements():
+        if (size[el] >= job.nb_machines):
+            return el
+    return max(size)
+
+def idx_less_of_eq(arr, val):
+    for i in range(len(arr)):
+        if arr[i] <= val:
+            return i
+
+def schedule(nb_machines, jobs):
     res = []
-    last_pos = [0 for i in range(m)]
-    for line in t:
-        if (get_available(t, m, deb) >= line.s):
-            line.start =
-            res.append(line)
-            for i in
-            last_pos[]
-        else:
-            deb += sort_size(res)[0].nb_machine
+    last_pos = [0 for i in range(nb_machines)]
+    for job in jobs:
+        pos = best_pos(job, last_pos)
+        job.start = pos
+
+        for i in range(job.nb_machines):
+            idx = idx_less_of_eq(last_pos, pos)
+            job.machines.append(idx)
+            last_pos[idx] += job.length
+
+        res.append(job)
+    return res
 
 if __name__ == '__main__':
     if (len(sys.argv) != 2):
         print("Usage : algo.py <file>")
         sys.exit()
     nb_machine, nb_jobs, data = get_data(sys.argv[1])
-    print(sort_length(data))
+    jobs = schedule(nb_machine, data)
+
+    for job in jobs:
+        print("{}".format(job.start))
